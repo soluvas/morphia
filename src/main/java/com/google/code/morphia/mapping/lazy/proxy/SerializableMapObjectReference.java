cdec9d4810f4b5d02936f93ab679a04e6cfe09a4
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-20 17:48:34 +0000

    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@335 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/main/java/com/google/code/morphia/mapping/lazy/proxy/SerializableMapObjectReference.java
+++ src/main/java/com/google/code/morphia/mapping/lazy/proxy/SerializableMapObjectReference.java
@@ -39,6 +39,8 @@ ProxiedEntityMap {
 	protected Object fetch() {
 		Map m = (Map) object;
 		m.clear();
+		// FIXME us: change to getting them all at once and yell according to
+		// ignoreMissing.
 		for (Map.Entry<?, String> e : keyMap.entrySet()) {
 			String entityKey = e.getValue();
 			Object entity = fetch(entityKey);
