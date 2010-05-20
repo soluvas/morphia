5b5ebfac842f8bdfb556dc83e306c4fe5913dda7
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-20 15:37:13 +0000

    cleanup and (yet untested Map support)
    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@329 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/test/java/com/google/code/morphia/mapping/lazy/TestLazySingleReference.java
+++ src/test/java/com/google/code/morphia/mapping/lazy/TestLazySingleReference.java
@@ -1,80 +1,83 @@
 package com.google.code.morphia.mapping.lazy;
 
+import com.google.code.morphia.Key;
 import com.google.code.morphia.annotations.Reference;
+import com.google.code.morphia.mapping.lazy.proxy.LazyReferenceFetchingException;
+import com.google.code.morphia.mapping.lazy.proxy.ProxiedEntityReference;
 import com.google.code.morphia.utils.AbstractMongoEntity;
 
 public class TestLazySingleReference extends ProxyTestBase {
 
-	// public final void testCreateProxy() {
-	// RootEntity root = new RootEntity();
-	// ReferencedEntity referenced = new ReferencedEntity();
-	//
-	// root.r = referenced;
-	// root.r.setFoo("bar");
-	//
-	// ds.save(referenced);
-	// ds.save(root);
-	//
-	// root = ds.get(root);
-	//
-	// assertNotFetched(root.r);
-	// assertEquals("bar", root.r.getFoo());
-	// assertFetched(root.r);
-	// assertEquals("bar", root.r.getFoo());
-	//
-	// // now remove it from DB
-	// ds.delete(root.r);
-	//
-	// root = deserialize(root);
-	// assertNotFetched(root.r);
-	//
-	// try {
-	// // must fail
-	// root.r.getFoo();
-	// fail("Expected Exception did not happen");
-	// } catch (LazyReferenceFetchingException expected) {
-	// // fine
-	// }
-	//
-	// }
-	//
-	// public final void testShortcutInterface() {
-	// RootEntity root = new RootEntity();
-	// ReferencedEntity reference = new ReferencedEntity();
-	//
-	// root.r = reference;
-	// reference.setFoo("bar");
-	//
-	// Key<ReferencedEntity> k = ds.save(reference);
-	// String keyAsString = k.getId().toString();
-	// ds.save(root);
-	//
-	// root = ds.get(root);
-	//
-	// ReferencedEntity p = root.r;
-	//
-	// assertIsProxy(p);
-	// assertNotFetched(p);
-	// assertEquals(keyAsString, ((ProxiedEntityReference) p).__getEntityId());
-	// // still unfetched?
-	// assertNotFetched(p);
-	// p.getFoo();
-	// // should be fetched now.
-	// assertFetched(p);
-	//
-	// root = deserialize(root);
-	// p = root.r;
-	// assertNotFetched(p);
-	// p.getFoo();
-	// // should be fetched now.
-	// assertFetched(p);
-	//
-	// root = ds.get(root);
-	// p = root.r;
-	// assertNotFetched(p);
-	// ds.save(root);
-	// assertNotFetched(p);
-	// }
+	public final void testCreateProxy() {
+		RootEntity root = new RootEntity();
+		ReferencedEntity referenced = new ReferencedEntity();
+
+		root.r = referenced;
+		root.r.setFoo("bar");
+
+		ds.save(referenced);
+		ds.save(root);
+
+		root = ds.get(root);
+
+		assertNotFetched(root.r);
+		assertEquals("bar", root.r.getFoo());
+		assertFetched(root.r);
+		assertEquals("bar", root.r.getFoo());
+
+		// now remove it from DB
+		ds.delete(root.r);
+
+		root = deserialize(root);
+		assertNotFetched(root.r);
+
+		try {
+			// must fail
+			root.r.getFoo();
+			fail("Expected Exception did not happen");
+		} catch (LazyReferenceFetchingException expected) {
+			// fine
+		}
+
+	}
+
+	public final void testShortcutInterface() {
+		RootEntity root = new RootEntity();
+		ReferencedEntity reference = new ReferencedEntity();
+
+		root.r = reference;
+		reference.setFoo("bar");
+
+		Key<ReferencedEntity> k = ds.save(reference);
+		String keyAsString = k.getId().toString();
+		ds.save(root);
+
+		root = ds.get(root);
+
+		ReferencedEntity p = root.r;
+
+		assertIsProxy(p);
+		assertNotFetched(p);
+		assertEquals(keyAsString, ((ProxiedEntityReference) p).__getEntityId());
+		// still unfetched?
+		assertNotFetched(p);
+		p.getFoo();
+		// should be fetched now.
+		assertFetched(p);
+
+		root = deserialize(root);
+		p = root.r;
+		assertNotFetched(p);
+		p.getFoo();
+		// should be fetched now.
+		assertFetched(p);
+
+		root = ds.get(root);
+		p = root.r;
+		assertNotFetched(p);
+		ds.save(root);
+		assertNotFetched(p);
+	}
 
 
 	public final void testSameProxy() {
