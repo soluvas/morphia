9bcf0d89ff1ebf2ff3ca8b2573a2affb4eaf3337
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-21 13:10:18 +0000

    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@339 7223e496-fac8-11de-aa3c-59de0406b4f5

---
+++ src/test/java/com/google/code/morphia/issue50/TestIdTwice.java
@@ -0,0 +1,31 @@
+package com.google.code.morphia.issue50;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import com.google.code.morphia.TestBase;
+import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.mapping.validation.ConstraintViolationException;
+import com.google.code.morphia.utils.AbstractMongoEntity;
+
+public class TestIdTwice extends TestBase {
+
+	@Test
+	public final void testRedundantId() {
+		try {
+			morphia.map(A.class);
+			fail();
+		} catch (ConstraintViolationException expected) {
+			// fine
+		}
+	}
+
+	public static class A extends AbstractMongoEntity {
+		@Id
+		String extraId;
+		@Id
+		String broken;
+	}
+
+}
