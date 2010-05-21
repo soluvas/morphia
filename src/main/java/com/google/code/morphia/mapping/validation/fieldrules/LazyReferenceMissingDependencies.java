5a26b919200a7b3c2f0401e3635a745a9898509a
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-21 16:36:22 +0000

    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@351 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/main/java/com/google/code/morphia/mapping/validation/fieldrules/LazyReferenceMissingDependencies.java
+++ src/main/java/com/google/code/morphia/mapping/validation/fieldrules/LazyReferenceMissingDependencies.java
@@ -24,7 +24,7 @@ public class LazyReferenceMissingDependencies extends FieldConstraint {
 		if (ref != null) {
 			if (ref.lazy()) {
 				if (!LazyFeatureDependencies.testDependencyFullFilled())
-					ve.add(new ConstraintViolation(Level.FATAL, mc, mf,
+					ve.add(new ConstraintViolation(Level.SEVERE, mc, mf,
 							"Lazy references need CGLib and Proxytoys in the classpath."));
 			}
 		}
