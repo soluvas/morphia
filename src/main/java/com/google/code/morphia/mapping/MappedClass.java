6d143653b8b483b886431dd6582f4ddffd0c4d3a
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-21 14:03:36 +0000

    refactored validation out to make it simple to test in isolation
    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@343 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/main/java/com/google/code/morphia/mapping/MappedClass.java
+++ src/main/java/com/google/code/morphia/mapping/MappedClass.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.code.morphia.EntityInterceptor;
@@ -234,48 +233,71 @@ public class MappedClass {
 		new MappingValidator().validate(this);
 
 		// No @Entity with @Embedded
-        if (getEntityAnnotation() != null && getEmbeddedAnnotation() != null ) {
-            throw new MappingException(
-                    "In [" + getClazz().getName()
-                           + "]: Cannot have both @Entity and @Embedded annotation at class level.");
-        }
+		//
+		// moved to EntityOrEmbed
+		//
+		// if (getEntityAnnotation() != null && getEmbeddedAnnotation() != null
+		// ) {
+		// throw new MappingException(
+		// "In [" + getClazz().getName()
+		// +
+		// "]: Cannot have both @Entity and @Embedded annotation at class level.");
+		// }
 
-        for (MappedField mf : persistenceFields) {
-            if (log.isLoggable(Level.FINE)) {
-                log.finer("Processing field: " + mf.getFullName());
-            }
-            
-            //a field can be a Value, Reference, or Embedded
-            if ( mf.hasAnnotation(Property.class) ) {
-                // make sure that the property type is supported
-                if (mf.isSingleValue() && !mf.isTypeMongoCompatible()) {
-                    throw new MappingException(mf.getFullName()
-                            + " is annotated as @Property but is a type that cannot be mapped (type is "
-                            + mf.getType().getName() + ").");
-                }
-            } else if (mf.hasAnnotation(Reference.class)) {
-            	
-            	Class realType = (mf.isSingleValue()) ? mf.getType() : mf.getSubType();
-            	if ((!realType.isInterface() && mapr.getMappedClass(realType).getIdField()==null))
-                    throw new MappingException(mf.getFullName()
-                            + " is annotated as a @Reference but the " + mf.getType().getName() + " class is missing the @Id annotation");
-            }            
-        }
+		
+		// for (MappedField mf : persistenceFields) {
+		// if (log.isLoggable(Level.FINE)) {
+		// log.finer("Processing field: " + mf.getFullName());
+		// }
+		//            
+		// //a field can be a Value, Reference, or Embedded
+		// if ( mf.hasAnnotation(Property.class) ) {
+		// // make sure that the property type is supported
+		// if (mf.isSingleValue() && !mf.isTypeMongoCompatible()) {
+		// throw new MappingException(mf.getFullName()
+		// +
+		// " is annotated as @Property but is a type that cannot be mapped (type is "
+		// + mf.getType().getName() + ").");
+		// }
+		// } else if (mf.hasAnnotation(Reference.class)) {
+		//            	
+		// Class realType = (mf.isSingleValue()) ? mf.getType() :
+		// mf.getSubType();
+		// if ((!realType.isInterface() &&
+		// mapr.getMappedClass(realType).getIdField()==null))
+		// throw new MappingException(mf.getFullName()
+		// + " is annotated as a @Reference but the " + mf.getType().getName() +
+		// " class is missing the @Id annotation");
+		// }
+		// }
         
         //Only embedded class can have no id field
-        if (getIdField() == null && getEmbeddedAnnotation() == null) {
-            throw new MappingException("In [" + getClazz().getName() + "]: No field is annotated with @Id; but it is required");
-        }
+		//
+		// moved to NoId
+		//
+		// if (getIdField() == null && getEmbeddedAnnotation() == null) {
+		// throw new MappingException("In [" + getClazz().getName() +
+		// "]: No field is annotated with @Id; but it is required");
+		// }
         
         //Embedded classes should not have an id
-        if (getEmbeddedAnnotation() != null && getIdField() != null) {
-            throw new MappingException("In [" + getClazz().getName() + "]: @Embedded classes cannot specify a @Id field");
-        }
+		//
+		// moved to EmbeddedAndId
+		//        
+		// if (getEmbeddedAnnotation() != null && getIdField() != null) {
+		// throw new MappingException("In [" + getClazz().getName() +
+		// "]: @Embedded classes cannot specify a @Id field");
+		// }
 
         //Embedded classes can not have a fieldName value() specified
-        if (getEmbeddedAnnotation() != null && !getEmbeddedAnnotation().value().equals(Mapper.IGNORED_FIELDNAME)) {
-            throw new MappingException("In [" + getClazz().getName() + "]: @Embedded classes cannot specify a fieldName value(); this is on applicable on fields");
-        }
+		//
+		// moved to EmbeddedAndValue
+		//  
+		// if (getEmbeddedAnnotation() != null &&
+		// !getEmbeddedAnnotation().value().equals(Mapper.IGNORED_FIELDNAME)) {
+		// throw new MappingException("In [" + getClazz().getName() +
+		// "]: @Embedded classes cannot specify a fieldName value(); this is on applicable on fields");
+		// }
 	}
 	
 	@Override
@@ -406,5 +428,9 @@ public class MappedClass {
 	
 	public Constructor getCTor() {
 		return ctor;
+	}
+	
+	public Mapper getMapper(){
+		return mapr;
 	}
 }
