c3dcc79d01a8b4d96d862eb2bc32198e26b6b902
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-23 15:16:17 +0000

    testcase for both, compressed & uncompressed
    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@373 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/test/java/com/google/code/morphia/mapping/validation/classrules/TestSerializedMap.java
+++ src/test/java/com/google/code/morphia/mapping/validation/classrules/TestSerializedMap.java
@@ -18,7 +18,13 @@ import com.google.code.morphia.utils.AbstractMongoEntity;
 public class TestSerializedMap extends JUnit3TestBase {
 	
 	public static class Map1 extends AbstractMongoEntity {
-		@Serialized()
+		@Serialized(disableCompression = false)
+		Map<Integer, Foo> shouldBeOk = new HashMap();
+		
+	}
+	
+	public static class Map2 extends AbstractMongoEntity {
+		@Serialized(disableCompression = true)
 		Map<Integer, Foo> shouldBeOk = new HashMap();
 		
 	}
@@ -42,6 +48,19 @@ public class TestSerializedMap extends JUnit3TestBase {
 		
 		assertEquals("peter", map1.shouldBeOk.get(3).id);
 		assertEquals("paul", map1.shouldBeOk.get(27).id);
+		
+	}
+	
+	public void testSerialization2() throws Exception {
+		Map2 map2 = new Map2();
+		map2.shouldBeOk.put(3, new Foo("peter"));
+		map2.shouldBeOk.put(27, new Foo("paul"));
+		
+		ds.save(map2);
+		map2 = ds.get(map2);
+		
+		assertEquals("peter", map2.shouldBeOk.get(3).id);
+		assertEquals("paul", map2.shouldBeOk.get(27).id);
 		
 	}
 }
