d4b26f7e02dcb114bfe41ce0f9a0dd9e728ce4e1
us@thomas-daily.de <us@thomas-daily.de@7223e496-fac8-11de-aa3c-59de0406b4f5>
2010-05-28 18:18:40 +0000

    git-svn-id: http://morphia.googlecode.com/svn/branches/lazy-refs@445 7223e496-fac8-11de-aa3c-59de0406b4f5

--- src/test/java/com/google/code/morphia/mapping/lazy/TestReferenceCollection.java
+++ src/test/java/com/google/code/morphia/mapping/lazy/TestReferenceCollection.java
@@ -1,7 +1,10 @@
 package com.google.code.morphia.mapping.lazy;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import junit.framework.Assert;
 
@@ -10,77 +13,103 @@ import org.junit.Test;
 import com.google.code.morphia.annotations.Reference;
 import com.google.code.morphia.utils.AbstractEntity;
 
-public class TestReferenceCollection extends ProxyTestBase
-{
+public class TestReferenceCollection extends ProxyTestBase {
 	@Test
-    public final void testCreateProxy()
-    {
-        A a = new A();
-        B b1 = new B();
-        B b2 = new B();
-
-        a.bs.add(b1);
-        a.bs.add(b2);
-
-        Collection<B> lazyBs = a.lazyBs;
-        lazyBs.add(b1);
-        lazyBs.add(b2);
-
-        ds.save(b2, b1, a);
-
-        a = ds.get(a);
-        
-        lazyBs = a.lazyBs;
-        Assert.assertNotNull(lazyBs);
+	public final void testCreateProxy() {
+		A a = new A();
+		B b1 = new B();
+		B b2 = new B();
+		
+		a.bs.add(b1);
+		a.bs.add(b2);
+		
+		Collection<B> lazyBs = a.lazyBs;
+		lazyBs.add(b1);
+		lazyBs.add(b2);
+		
+		ds.save(b2, b1, a);
+		
+		a = ds.get(a);
+		
+		lazyBs = a.lazyBs;
+		Assert.assertNotNull(lazyBs);
 		assertNotFetched(lazyBs);
-
-        Assert.assertNotNull(lazyBs.iterator().next());
+		
+		Assert.assertNotNull(lazyBs.iterator().next());
 		assertFetched(lazyBs);
-
-        a = deserialize(a);
-
-        lazyBs = a.lazyBs;
-        Assert.assertNotNull(lazyBs);
+		
+		a = deserialize(a);
+		
+		lazyBs = a.lazyBs;
+		Assert.assertNotNull(lazyBs);
 		assertNotFetched(lazyBs);
-
-        Assert.assertNotNull(lazyBs.iterator().next());
+		
+		Assert.assertNotNull(lazyBs.iterator().next());
 		assertFetched(lazyBs);
-
-        a = deserialize(a);
-
-        ds.save(a);
-        lazyBs = a.lazyBs;
+		
+		a = deserialize(a);
+		
+		ds.save(a);
+		lazyBs = a.lazyBs;
 		assertNotFetched(lazyBs);
-    }
+	}
+	
+	@Test
+	public void testOrderingPreserved() throws Exception {
+		A a = new A();
+		B b1 = new B();
+		b1.setFoo("b1");
+		a.lazyBs.add(b1);
+		
+		B b2 = new B();
+		b2.setFoo("b2");
+		a.lazyBs.add(b2);
+		ds.save(b1);
+		ds.save(b2);
+		
+		Assert.assertEquals("b1", a.lazyBs.iterator().next().foo);
+		
+		ds.save(a);
 
-   
+		A reloaded = ds.get(a);
+		Assert.assertEquals("b1", reloaded.lazyBs.iterator().next().foo);
+		Collections.swap((List<B>) reloaded.lazyBs, 0, 1);
+		Assert.assertEquals("b2", reloaded.lazyBs.iterator().next().foo);
+		
+		ds.save(reloaded);
 
-    public static class A extends AbstractEntity
-    {
-		{
-			System.out.println(this.getClass().getName());
+		reloaded = ds.get(reloaded);
+		Collection<B> lbs = reloaded.lazyBs;
+		Assert.assertEquals(2, lbs.size());
+		Iterator<B> iterator = lbs.iterator();
+		Assert.assertEquals("b2", iterator.next().foo);
+		
+	}
+	
+	public static class A extends AbstractEntity {
+		@Reference(lazy = false)
+		Collection<B> bs = new LinkedList();
+
+		@Reference(lazy = true)
+		Collection<B> lazyBs = new LinkedList();
+		
+	}
+	
+	public static class B extends AbstractEntity {
+		private String foo;
+		
+		public void setFoo(final String string) {
+			this.foo = string;
 		}
-        @Reference
-        Collection<B> bs = new LinkedList();
+		
+		public String getFoo() {
+			return this.foo;
+		}
 
-        @Reference(lazy = true)
-        Collection<B> lazyBs = new LinkedList();
-
-    }
-
-    public static class B extends AbstractEntity
-    {
-        private String foo;
-
-        public void setFoo(final String string)
-        {
-            this.foo = string;
-        }
-
-        public String getFoo()
-        {
-            return this.foo;
-        }
-    }
+		@Override
+		public String toString() {
+			return super.toString() + " : id = " + id + ", foo=" + foo;
+		}
+	}
 
 }
